a6eec013c2fb7ba272e71cc66456dedc565e1fb4
vdovaT <vdova@protonmail.com>
2022-02-07 14:35:55 +0800

    update

--- src/components/earn/SidePoolItem.tsx
+++ src/components/earn/SidePoolItem.tsx
@@ -74,8 +74,8 @@ export default function SidePoolItem({
   // get the color of the token
   const backgroundColor = useColor(token)
   // TODO: Orange NEED FIX
-  // const totalStakedInUsd = stakingInfo.totalStakedInUsd.toSignificant(4, { groupSeparator: ',' })
-  const totalStakedInUsd = '-'
+  const totalStakedInUsd = stakingInfo.totalStakedInUsd.toSignificant(4, { groupSeparator: ',' })
+  // const totalStakedInUsd = '-'
   const pairAddress = stakingInfo?.stakedAmount?.token?.address
   return (
     <StyledInternalLink
