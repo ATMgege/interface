8e673435c5d82add76083daa75fd25d49d89f8fb
vdovaT <vdova@protonmail.com>
2022-04-05 02:49:23 +0800

    udpate

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,8 +1,9 @@
 // the Pangolin Default token list lives here
 export const AEB_TOKENLIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/aeb.tokenlist.json'
 
-export const COIN_LIST = 'https://api.coinmarketcap.com/data-api/v3/uniswap/all.json'
+export const COIN_LIST =
+  'https://gist.githubusercontent.com/vdovaT/d181aa6ae6ef6253209fc74d94eae39b/raw/6b7887588410e1623090d8891114f9f77b6cdc0d/tokenlist.json'
 
 export const DEFAULT_TOKEN_LISTS_SELECTED: string[] = [COIN_LIST]
 
-export const DEFAULT_TOKEN_LISTS: string[] = []
+export const DEFAULT_TOKEN_LISTS: string[] = [COIN_LIST]
